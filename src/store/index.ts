27d7103310ba8614b9d985254006fcd0b47cb961
Philipp Walter <philippwalter@proton.me>
2023-10-13 16:03:20 +0200

    fix(receive): add missing migration

--- src/store/index.ts
+++ src/store/index.ts
@@ -37,7 +37,7 @@ const persistConfig = {
 	key: 'root',
 	storage: mmkvStorage,
 	// increase version after store shape changes
-	version: 24,
+	version: 25,
 	stateReconciler: autoMergeLevel2,
 	blacklist: ['receive', 'ui'],
 	migrate: createMigrate(migrations, { debug: __ENABLE_MIGRATION_DEBUG__ }),
