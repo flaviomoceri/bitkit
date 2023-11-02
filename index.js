b25f0e42052bab4a4e6312258189d0cd5176406f
Philipp Walter <philippwalter@proton.me>
2023-11-02 10:28:54 +0100

    fix: enable react-native-fetch-api polyfill

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fet
 import { polyfill as polyfillEncoding } from 'react-native-polyfill-globals/src/encoding';
 import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
 import './src/utils/ignoreLogs';
+import './src/utils/fetch';
 import { AppRegistry, Text, TextInput } from 'react-native';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import Root from './src/Root';
 import { name as appName } from './app.json';
 
-// Polyfill fetch streaming on iOS (Android is not supported yet)
-// https://github.com/react-native-community/fetch/issues/13
+// Polyfill fetch streaming
 polyfillEncoding();
 polyfillReadableStream();
 polyfillFetch();
